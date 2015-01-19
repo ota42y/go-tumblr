17e4edeb3b71fbb8a663c59f2ec115dcd7f64f6b
ota42y <ota42y@gmail.com>
2015-01-20 07:35:57 +0900

    add blog info response

---
+++ tumblr/blog_api.go
@@ -0,0 +1,35 @@
+package tumblr
+
+
+import (
+	"net/http"
+	"net/url"
+	"encoding/json"
+)
+
+type BlogApi struct{
+	Host string
+	client *Client
+}
+
+func NewBlogApi(host string, client *Client) (*BlogApi){
+	return &BlogApi{
+		Host: host,
+		client: client,
+	}
+}
+
+func (blog *BlogApi) Info() (m *Meta, b *Blog, err error){
+	values := url.Values{}
+	values.Add("api_key", blog.client.ConsumerKey)
+
+	uri := "http://api.tumblr.com/v2/blog/" + blog.Host + "/info?" + values.Encode()
+
+	res, err := http.Get(uri)
+
+	var r Root
+	dec := json.NewDecoder(res.Body)
+	dec.Decode(&r)
+
+	return &r.Meta, &r.Response.Blog, err
+}
---
+++ tumblr/blog_api_test.go
@@ -0,0 +1,40 @@
+package tumblr
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	client := NewClient("", "", "", "")
+	actual := NewBlogApi("scipsy.tumblr.com", client)
+	if actual == nil {
+		t.Errorf("got %v\nwant %v", actual, nil)
+	}
+}
+
+func TestInfo(t *testing.T) {
+	client := NewClient("", "", "", "")
+	blogApi := NewBlogApi("scipsy.tumblr.com", client)
+	meta, blog, err := blogApi.Info()
+
+	if err != nil{
+		t.Errorf("response error%v\n", err)
+	}
+
+	if meta == nil{
+		t.Errorf("got %v\nwant %v error is %v\n", meta, nil, err)
+	}
+
+	if blog == nil{
+		t.Errorf("got %v\nwant %v error is %v\n", blog, nil, err)
+	}
+
+	validCode := 200
+	if meta.Status != validCode{
+		t.Errorf("%v is not response code %v", meta.Status, validCode)
+	}
+
+	if blog.Title == ""{
+		t.Errorf("%v is not valid response", blog.Title)
+	}
+}
